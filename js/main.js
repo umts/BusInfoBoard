27573cd80a8908755f02fffba85c8bde1fbebd40
Brandon Sprague <brandon00sprague@gmail.com>
2014-11-12 15:20:16 -0500

    Loading from a config file

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 // Javascript for parsing and displaying departure information
 var routes = {};
-var url = "http://bustracker.pvta.com/InfoPoint/rest/";
+
+var config_url = "http://localhost:8000/config.json";
+
+var options =
+{
+  url: "http://bustracker.pvta.com/InfoPoint/rest/",
+  stops: [],
+  allowed_routes: [],
+  excluded_trips: [],
+  start_animation_type: 'fadeInDown',  // default animate CSS for each row to be added with
+  end_animation_type: 'fadeOut',       // default animate CSS for everything to be removed with at once
+  work_day_start: 4,
+  refresh_time: 30000, // default time in ms between refreshes
+}
 var container;
 var refresh_id;
 var sort_function;
-var stops = [];
-var allowed_routes = [];
-var excluded_trips = [];
-var start_animation_type = 'fadeInDown'; // default animate CSS for each row to be added with
-var end_animation_type = 'fadeOut'; // default animate CSS for everything to be removed with at once
 var stop_index = 0;
-var work_day_start = 4;
-var REFRESH_TIME = 30000; // default time in ms between refreshes
+
 var MINIMUM_REFRESH_TIME = 5; // minimum number of seconds allowed for user input
 var CASCADE_SPEED = 75; // time in ms which each row will take to cascade
 var END_ANIMATION_TIME = 500; // the amount of time we give the ending animate CSS to work
+
 // The timezone at the beginning of the current day, used for making sure ETAs don't become
 // incorrect when switching between DST and...not DST.
 var dst_at_start;
@@ -33,7 +41,7 @@ function QueryStringAsObject() {
 
 $(function(){
   container = $('.container-fluid');
-  parseQueryString();
+  updateOptions();
   initBoard();
 });
 
@@ -43,7 +51,7 @@ function startErrorRoutine() {
     container.append('<div class="connectivity_note">No Bus Information Available</div>');
   }
   $.ajax({
-    url: url + "PublicMessages/GetCurrentMessages",
+    url: options.url + "PublicMessages/GetCurrentMessages",
     success: function(route_data) {
       container.empty();
       initBoard();
@@ -54,31 +62,44 @@ function startErrorRoutine() {
   });
 }
 
-function parseQueryString() {
+function updateOptions() {
+  // First load from a config_url if one is specified
+  if (typeof config_url !== "undefined") {
+    $.ajax({
+      url: config_url,
+      dataType: 'json',
+      success: function(new_options) {
+        $.extend(options, new_options);
+      },
+      async: false,
+      timeout: 1000
+    });
+  }
+
   var query = QueryStringAsObject();
+
   var stop_query_string = query.stops;
   // Check if a query string has been specified
   if (typeof stop_query_string !== "undefined") {
     var query_parts = stop_query_string.split("+");
     for (var i = 0; i < query_parts.length; i++) {
       if (query_parts[i]) {
-        stops.push(query_parts[i]);
+        options.stops.push(query_parts[i]);
       }
     }
   }
   // If they didn't enter any stops, or they screwed up, default to stop ID 64, the Integrative Learning Center
-  if (stops.length == 0) {
-    stops = [64];
+  if (options.stops.length == 0) {
+    options.stops = [64];
   }
 
   var route_query_string = query.routes;
 
   if (typeof route_query_string !== "undefined") {
-    routes = [];
     var query_parts = route_query_string.split("+");
     for (var i = 0; i < query_parts.length; i++) {
       if (query_parts[i]) {
-        allowed_routes.push(query_parts[i]);
+        options.allowed_routes.push(query_parts[i]);
       }
     }
   }
@@ -95,16 +116,16 @@ function parseQueryString() {
 
   var start_animation_query_string = query.start_animation;
   if (typeof start_animation_query_string !== 'undefined'){
-    start_animation_type = start_animation_query_string
+    options.start_animation_type = start_animation_query_string
   }
 
   var end_animation_query_string = query.end_animation;
   if (typeof end_animation_query_string !== 'undefined'){
-    end_animation_type = end_animation_query_string
+    options.end_animation_type = end_animation_query_string
   }
 
   //if there are no animations specified, don't allow time for them to execute
-  if (start_animation_type == 'none' && end_animation_type == 'none'){
+  if (options.start_animation_type == 'none' && options.end_animation_type == 'none'){
     END_ANIMATION_TIME = 0
   }
 
@@ -112,8 +133,7 @@ function parseQueryString() {
   //minimum allowed is MINIMUM_REFRESH_TIME seconds
   var interval_query_string = query.interval;
   if (typeof interval_query_string !== 'undefined'){
-    user_value = parseInt(interval_query_string)
-    REFRESH_TIME = Math.max(MINIMUM_REFRESH_TIME, user_value) * 1000;
+    options.refresh_time = Math.max(MINIMUM_REFRESH_TIME, parseInt(interval_query_string)) * 1000;
   }
 
   var excluded_query_string = query.excluded_trips;
@@ -122,14 +142,14 @@ function parseQueryString() {
     var query_parts = excluded_query_string.split("+");
     for (var i = 0; i < query_parts.length; i++) {
       if (query_parts[i]) {
-        excluded_trips.push(query_parts[i]);
+        options.excluded_trips.push(query_parts[i]);
       }
     }
   }
 
   var work_day_start_string = query.work_day_start;
   if (typeof work_day_start_string !== "undefined") {
-    work_day_start = parseInt(work_day_start_string) % 24;
+    options.work_day_start = parseInt(work_day_start_string) % 24;
   }
 }
 
@@ -137,7 +157,7 @@ function initBoard() {
   // Let's start by preloading all of the route info, because when we query
   // departures, we'll only have the route ID
   $.ajax({
-    url: url + "routes/getvisibleroutes",
+    url: options.url + "routes/getvisibleroutes",
     success: function(route_data) {
     for (var i = 0; i < route_data.length; i++) {
       routes[route_data[i].RouteId] = route_data[i];
@@ -158,7 +178,7 @@ function startRefreshing() {
     setTimeout(function(){
       addTables();
     }, END_ANIMATION_TIME)
-  }, REFRESH_TIME);
+  }, options.refresh_time);
 }
 
 function stopRefreshing() {
@@ -168,20 +188,20 @@ function stopRefreshing() {
 function addTables() {
   var current = moment();
   // Is it still "yesterday" as far as the transit agency is concerned
-  if (current.hour() <= work_day_start) {
-    dst_at_start = moment([current.year(), current.month(), current.date(), work_day_start]).subtract(1, "days").isDST();
+  if (current.hour() <= options.work_day_start) {
+    dst_at_start = moment([current.year(), current.month(), current.date(), options.work_day_start]).subtract(1, "days").isDST();
   } else {
-    dst_at_start = moment([current.year(), current.month(), current.date(), work_day_start]).isDST();
+    dst_at_start = moment([current.year(), current.month(), current.date(), options.work_day_start]).isDST();
   }
 
   var row, section;
   var size_class = "";
-  if (stops.length > 1) {
+  if (options.stops.length > 1) {
     size_class = "col-lg-12";
   }
   // There are two separate calls here, 
   $.ajax({
-    url: url + "stops/get/"+stops[stop_index],
+    url: options.url + "stops/get/" + options.stops[stop_index],
     success: function(stop_info) {
       if (stop_index % 2 == 0) {
         row = $('<div class="row route-holder"></div>');
@@ -194,14 +214,14 @@ function addTables() {
         container.append(row);
       }
       $.ajax({
-        url: url + "stopdepartures/get/" + stops[stop_index],
+        url: options.url + "stopdepartures/get/" + options.stops[stop_index],
         success: function(departure_data) {
           
           // Draw the header for each stop
-          section.append('<h1 class="animated ' + start_animation_type + '">' + stop_info.Name + "</h1>");
+          section.append('<h1 class="animated ' + options.start_animation_type + '">' + stop_info.Name + "</h1>");
           var infos = getDepartureInfo(departure_data[0].RouteDirections);
           if (infos.length == 0){
-            section.append('<h2 class="animated ' + start_animation_type + '">No remaining scheduled departures.</h2>');
+            section.append('<h2 class="animated ' + options.start_animation_type + '">No remaining scheduled departures.</h2>');
           }
           var i = 0; // For that soothing cascading effect
           var id = setInterval(function() {
@@ -213,8 +233,8 @@ function addTables() {
                 clearTimeout(id);
                 stop_index++;
                 // If we run out of stops, reset our index and don't do anything
-                if (stop_index < stops.length) {
-                  addTables(stops);
+                if (stop_index < options.stops.length) {
+                  addTables(options.stops);
                 } else {
                   stop_index = 0;
                 }
@@ -231,8 +251,8 @@ function addTables() {
 //removes the tables in preparation to load in the new ones. fancy CSS magic.
 function removeTables(){
   //fade out stops and their departures
-  $('h1').addClass(end_animation_type);
-  $('.route').addClass(end_animation_type);
+  $('h1').addClass(options.end_animation_type);
+  $('.route').addClass(options.end_animation_type);
   //once we've given that END_ANIMATION_TIME to work, remove everything.
   setTimeout(function(){
     container.empty()
@@ -248,7 +268,7 @@ function renderRow(info, section) {
   var short_proportions = "col-xs-24 col-sm-2 col-md-2 col-lg-1";
   var long_proportions = "col-xs-24 col-sm-15 col-md-15 col-lg-16";
   var arrival_proportions = "col-xs-24 col-sm-7 col-md-7 col-lg-7";
-  if (stops.length > 1) {
+  if (options.stops.length > 1) {
     short_proportions = "col-xs-24 col-sm-2 col-md-1 col-lg-2";
     long_proportions = "col-xs-24 col-sm-15 col-md-16 col-lg-15";
     arrival_proportions = "col-xs-24 col-sm-7 col-md-7 col-lg-7";
@@ -266,7 +286,7 @@ function renderRow(info, section) {
     }
   }
   section.append(
-      '<div class="route animated ' + start_animation_type + '" style="background-color: #' + info.Route.Color + '">' +
+      '<div class="route animated ' + options.start_animation_type + '" style="background-color: #' + info.Route.Color + '">' +
       '<div class="row">' + 
       '<div class="route_short_name ' + short_proportions + ' text-center-xs" style="color: #' + info.Route.TextColor + '">' +
       info.Route.ShortName + " " + 
@@ -299,9 +319,9 @@ function getDepartureInfo(directions) {
       //If the departure has a unique InternetServiceDesc,
       if ($.inArray(departure.Trip.InternetServiceDesc, unique_ISDs) == -1
           //and if it's in the allowed routes,
-          && (allowed_routes.length == 0 || $.inArray(route.ShortName, allowed_routes) != -1)
+          && (options.allowed_routes.length == 0 || $.inArray(route.ShortName, options.allowed_routes) != -1)
           //and if it's not in the excluded trips
-          && (excluded_trips.length == 0 || $.inArray(departure.Trip.InternetServiceDesc, excluded_trips) == -1)
+          && (options.excluded_trips.length == 0 || $.inArray(departure.Trip.InternetServiceDesc, options.excluded_trips) == -1)
           //and if it's in the future,
           && moment(departure.EDT).isAfter(Date.now())) {
         // then we push it to the list, and push its ISD to the unique ISDs list.
